37c82ed70610d673eed5643b417346a062e21ea3
ed.lin <ed.lin@esunergy.com>
2021-03-08 16:21:15 +0800

    示範內嵌繫結用法

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  sitename = 'Hello Angular 11';
+  sitelogo = '/assets/images/angular11_logo.png';
+  sitetitle2 = 'Angular 11 新手開發練功坊課程實作練習';
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.sitename = 'Hello Word';
+      this.sitetitle2 = '練習練習';
+    }, 2000);
   }
-
 }
