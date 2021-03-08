29134b9f955964ef1f7a7bbf439db4022fd3bbac
ed.lin <ed.lin@esunergy.com>
2021-03-08 17:17:09 +0800

    用一個很髒的解法解決一個難纏的 Bug 此Bug為Angular value無異動時產生

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   data = {
-    keyword: 'demo1'
+    keyword: 'demo1',
   };
+
+  doSearch(value: string): void {
+    this.data.keyword = value;
+    if (!value) {
+      this.data.keyword = ' ';
+      setTimeout(() => {
+        this.data.keyword = '';
+      }, 1);
+    }
+  }
 }
