11d4c4776f39c6a30cb92d77624ddc0fcdadfc63
ed.lin <ed.lin@esunergy.com>
2021-03-12 16:14:11 +0800

    嘗試用 Observable 搭配 async pipe 訂閱結果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from './data.service';
 import { Article } from './Article';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,16 @@ import { Article } from './Article';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  data: Article[] = [];
-
+  data$: Observable<Article[]> = of([]);
   search = {
     keyword: 'demo1',
   };
   constructor(private datasvc: DataService) {}
   ngOnInit(): void {
-    this.datasvc.loadArticles().subscribe((results) => {
-      this.data = results;
-    });
+    // this.datasvc.loadArticles().subscribe((results) => {
+    //   this.data = results;
+    // });
+    this.data$ = this.datasvc.loadArticles();
   }
   doSearch(value: string, elm?: HTMLInputElement): void {
     this.search.keyword = value;
